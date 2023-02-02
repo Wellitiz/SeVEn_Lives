cc6ad403d28d4c0229ef4b2aacd95406568e9832
izaias <izaiasenelly@gmail.com>
2023-02-02 19:55:23 -0300

    testing dynamic rendering

--- Sprint-IV/script.js
+++ Sprint-IV/script.js
@@ -89,60 +89,11 @@ const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
 
 // CRIA O ELEMENTO
-//const form = document.createElement("form");
-
+const about = document.createElement(div);
 //POPULA O ELEMENTO
-// form.innerHTML = `
-// <div class="xcontainer">
-//   <a class="links" id="paracadastro"></a>
-//   <a class="links" id="paralogin"></a>
-//   <div class="xcontent">
-//     <!--Login-->
-//     <div id="login">
-//       <form method="post" action="">
-//         <h1 class="h1_form">Login</h1>
-//         <p>
-//           <label for="email_login">Seu E-mail</label>
-//           <input id="login_email" name="login_email"required="required" type="text" placeholder="seuemail@email.com">
-//         </p>
-//         <p>
-//           <label for="senha_login">Sua Senha</label>
-//           <input id="senha_login" name="senha_login" required="required" type="password" placeholder="1234567">
-//         </p>
-//         <p>
-//           <input type="checkbox" name="manterlogado" id="manterlogado" value="">
-//           <label for="manterlogado">Manter-me Logado</label>
-//         </p>
-//         <p>
-//           <input type="submit" value="Logar" />
-//         </p>
-//           <p class="link">Ainda não tem conta?
-//           <a href="#paracadastro">Cadastre-se Aqui</a>
-//         </p>
-//       </form>
-//     </div>
-//     <!--Formulário Cadastro-->
-//     <div id="cadastro">
-//       <form method="post" action="">
-//         <h1 class="h1_form">Cadastro</h1>
-//         <p>
-//           <label for="nome_cad">Seu nome</label>
-//           <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder="Digite seu nome" />
-//         </p>
-//         <p>
-//           <label for="email_cad">Seu e-mail</label>
-//           <input id="email_cad" name="email_cad" required="required" type="email" placeholder="seuemail@email.com">
-//         </p>
-//         <p>
-//           <label for="senha_cad">Sua Senha</label>
-//           <input id="senha_cad" name="senha_cad" required="required" type="password" placeholder="Digite uma Senha Segura">
-//         </p>
-//         <p class="link">Já Possui conta? <a href="#paralogin"> Logar Conta</a></p>
-//       </form>
-//     </div>
-//   </div>
-// </div>
-// `;
-// // ADICIONA O ELEMENTO NO DOM
-// const main = document.querySelector("main");
-// main.appendChild(form);
+about.innerHTML = `
+<h1>Aqui foi renderizado</h1>
+`
+//ADICIONA O ELEMENTO NO DOM
+const main = document.querySelector('main');
+main.appendChild(about);
